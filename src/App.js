948f037dc93937daa4a070639de5975a99f3fd49
Manuel Fuentes <manuelfuentesj@gmail.com>
2021-10-25 16:44:51 -0300

    entrega do projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,253 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button, Container, Modal, ModalBody, ModalHeader, FormGroup, ModalFooter, Label, Input, Form} from "reactstrap";
+import Buscar from './components/Buscar';
+import { getAllContatos, criarContatos, editarContato, deletarContato } from './components/Api'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const data = [];
+
+class App extends React.Component {
+  state = {
+    data: data,
+    form: {
+      nome: "",
+      email:"",
+      telefone: "",
+      grupo: ""
+    }, 
+    dataSearch: [],
+    modalInserir: false,
+    modalAtualizar: false,
+    modalConfirmacao: false
+  };
+
+  
+
+  handleChange = e =>{
+    this.setState({
+      form:{
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      }
+    })
+  }
+ 
+  //#região metodos de modales
+  mostrarModalAtualizar = (valor) => {
+    this.setState({
+      form: valor,
+      modalAtualizar: true,
+    });
+  };
+
+  fecharModalAtualizar = () => {
+    this.setState({ modalAtualizar: false })
+
+  };
+
+  mostrarModalInserir = () => {
+    this.setState({
+      modalInserir: true,
+    });
+  };
+
+  fecharModalInserir = () => {
+    this.setState({ modalInserir: false });
+  };
+
+  mostrarConfirmacao = (valor) => {
+    this.setState({
+      form: valor,
+      modalConfirmacao: true
+    });
+    
+  };
+
+  fecharConfirmacao = () =>{
+    this.setState({ modalConfirmacao: false });
+  }
+
+  inserir = async() =>{
+    let valorNovo = this.state.form;
+    await criarContatos(valorNovo)
+    this.obterDados()
+
+  }
+
+  componentDidMount () {
+    this.obterDados ()
+  }
+
+  obterDados = async() => {
+    let contatos = await getAllContatos ()
+    this.setState({data: contatos.data, modalInserir: false})
+  }
+  
+ 
+  
+  editar = async(valor) =>{
+      const {id, ...data} = valor
+     await editarContato(id,data)
+    
+    this.setState({ modalAtualizar: false})
+    this.obterDados()
+  }
+
+
+
+  apagar = (valor) =>{
+    const {id} = valor 
+    deletarContato(id);
+      this.setState({modalConfirmacao: false});
+      this.obterDados()
+
+  }
+
+
+
+  buscar = (e) =>{
+    const { value } = e.target;
+    let lista = this.state.data;
+    const filtered = lista.filter(fltr => fltr.nome.toLowerCase().includes(value.toLowerCase()));
+    this.setState({ dataSearch: !value ?  [] : filtered});   
+  }
+  //#endregion
+
+  render() {  
+    return (
+      <>    
+        <nav className="navbar navbar-dark bg-dark">
+          <div className="container-fluid">
+            <h1 style={{ 
+              color: "white",
+              fontSize: "30px" }} className="navbar-brand">
+              Projeto React</h1>
+            <Form>
+            <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+              <div>
+                <Button color="success" onClick={()=>this.mostrarModalInserir()}>Adicionar novo contato</Button>
+              </div>
+              <div>
+                <input className="form-control me-2" onChange={this.filtrar} type="search" placeholder="Buscar" aria-label="Search"></input>
+              </div> 
+            </FormGroup>
+            </Form>
+            </div>
+        </nav>
+             
+          
+        <Container>             
+          <Buscar 
+            data = {this.state.dataSearch.length ? this.state.dataSearch : this.state.data}
+            apagar = {this.mostrarConfirmacao}
+            editar = {this.mostrarModalAtualizar}
+          />   
+          
+        </Container>
+
+        
+        
+        {/* Modal Inserir */}
+        <Modal isOpen = {this.state.modalInserir}>
+          <ModalHeader>
+            <div>
+              <h3>Adicionar contacto</h3>
+            </div>       
+          </ModalHeader>
+
+          <ModalBody>
+           
+            <FormGroup>
+              <label>Nome</label> 
+              <input className="form-control" name="nome" type= "text" onChange={this.handleChange}/>
+            </FormGroup> 
+
+            <FormGroup>
+              <label>Email</label> 
+              <input className="form-control" name="email" type= "text" onChange={this.handleChange}/>
+            </FormGroup> 
+
+            <FormGroup>
+              <label>Telefone</label> 
+              <input className="form-control" name="telefone" type= "text" onChange={this.handleChange}/>
+            </FormGroup> 
+  
+            <FormGroup>
+              <Label for="exampleSelect">Grupo</Label>
+              <Input type="select" name="grupo" id="exampleSelect" onChange={this.handleChange}>
+                <option value="" disabled selected>Selecione...</option>
+                <option>Familia</option>
+                <option>Amigos</option>
+                <option>Trabalho</option>
+                <option>Super-heróis</option>
+                <option>Vilões</option>
+              </Input>
+            </FormGroup>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button color="primary" onClick={() => this.inserir()}>Adicionar</Button>
+            <Button color="danger" onClick={() => this.fecharModalInserir()}>Cancelar</Button>
+          </ModalFooter>
+        </Modal>
+
+
+        {/* Modal atualizar */}
+        <Modal isOpen={this.state.modalAtualizar}>
+          <ModalHeader>
+            <div>
+                <h3>Editar contato</h3>
+            </div>       
+          </ModalHeader>
+
+          <ModalBody>
+
+            <FormGroup>
+              <label>Nome</label> 
+              <input className="form-control" name="nome" type= "text" onChange={this.handleChange} value={this.state.form.nome}/>
+            </FormGroup> 
+
+            <FormGroup>
+              <label>Email</label> 
+              <input className="form-control" name="email" type= "text" onChange={this.handleChange} value={this.state.form.email}/>
+            </FormGroup> 
+
+            <FormGroup>
+              <label>Telefone</label> 
+              <input className="form-control" name="telefone" type= "text" onChange={this.handleChange} value={this.state.form.telefone}/>
+            </FormGroup> 
+            <FormGroup>
+              <label>Grupo</label> 
+              <Input className="form-control" name="grupo" type= "select" onChange={this.handleChange} value={this.state.form.grupo}>
+                <option>Familia</option>
+                <option>Amigos</option>
+                <option>Trabalho</option>
+                <option>Super-heróis</option>
+                <option>Vilões</option>
+              </Input>
+            </FormGroup> 
+             
+          </ModalBody>
+
+          <ModalFooter>
+            <Button color="primary" onClick={()=>this.editar(this.state.form)}>Salvar</Button>
+            <Button color="danger" onClick={()=>this.fecharModalAtualizar()}>Cancelar</Button>
+          </ModalFooter>
+        </Modal>
+        
+        <Modal isOpen = {this.state.modalConfirmacao}>
+          <ModalHeader>
+            <h4> Tem certeza que deseja apagar o contato? </h4>
+          </ModalHeader>
+          <ModalBody>
+            <Button color="primary" onClick={()=> this.apagar(this.state.form)}> Sim</Button> {"   "}
+            <Button color="danger" onClick={()=>this.fecharConfirmacao()}> Não</Button>
+          </ModalBody>        
+        </Modal>
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Buscar.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Table, Button} from "reactstrap";
+  
+const buscar = ({data, apagar, editar}) =>{
+    return(
+        <Table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Email</th>
+                    <th>Telefone</th>
+                    <th>Grupo</th>
+                    <th class="text-center">Acões</th>
+                </tr>
+            </thead>
+            <tbody>
+            {data.map((elemento)=>(
+                <tr>
+                <td>{elemento.id}</td>
+                <td>{elemento.nome}</td>
+                <td>{elemento.email}</td>
+                <td>{elemento.telefone}</td>
+                <td>{elemento.grupo}</td>
+                <td class="text-center">
+                    <Button color="primary" onClick={() => editar(elemento)}>Editar</Button>{' '}
+                    <Button color="danger" onClick={() => apagar(elemento)}>Apagar</Button>
+                </td>
+                </tr>               
+            ))}
+            </tbody>
+            
+        </Table> 
+        
+    )
+}
+
+export default buscar;
+      
